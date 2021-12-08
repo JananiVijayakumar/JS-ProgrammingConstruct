ec4a555c236a588e8600e057dc4f05b595687e90
Janani <vsvprema@gmail.com>
2021-12-08 17:41:45 +0530

    Added: Addition of 2 random dice number

---
+++ Add2Random.js
@@ -0,0 +1,6 @@
+var dice1= Math.floor((Math.random() * 6 )+ 1);
+var dice2= Math.floor((Math.random() * 6 )+ 1);
+var Dice = dice1 + dice2;
+console.log("The Dice 1 value is : "+dice1);
+console.log("The Dice 1 value is : "+dice2);
+console.log("The addition of 2 random dice value is :"+Dice);
