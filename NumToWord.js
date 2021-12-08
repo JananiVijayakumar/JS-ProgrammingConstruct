a76200c11d34e2c30be0e7465b835ff11310d4aa
Janani <vsvprema@gmail.com>
2021-12-08 22:10:24 +0530

    Added: print the given number to word

---
+++ NumToWord.js
@@ -0,0 +1,35 @@
+var prompt = require('prompt-sync')();
+var number = prompt("Enter a number between 0 to 9: ");
+if ( number == 0 ) {
+    console.log("The number is Zero");
+}
+else if ( number == 1 ) {
+    console.log("The number is One");
+}
+else if ( number == 2 ) {
+    console.log("The number is Two");
+}
+else if ( number == 3 ) {
+    console.log("The number is Three");
+}
+else if ( number == 4 ) {
+    console.log("The number is Four");
+}
+else if ( number == 5 ) {
+    console.log("The number is Five");
+}
+else if ( number == 6 ) {
+    console.log("The number is Six");
+}
+else if ( number == 7 ) {
+    console.log("The number is Seven");
+}
+else if ( number == 8 ) {
+    console.log("The number is Eight");
+}
+else if ( number == 9 ) {
+    console.log("The number is Nine");
+}
+else {
+    console.log("Please enter a single digit number!!");
+}
